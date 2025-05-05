2d16cedb80623157cc55d03ef99488ba436e0376
schiz697 <varquez.elmerjr@gmail.com>
2025-05-05 19:27:14 +0800

    added alert api

---
+++ Api/Controllers/AlertController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlertController : ControllerBase
+    {
+        private static string _alertMessage = null;
+
+        [HttpPost("alert")]
+        public IActionResult PostAlert([FromBody] string status)
+        {
+            _alertMessage = status;
+            return Ok($"Alert received: {status}");
+        }
+
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            return Ok(_alertMessage ?? "NO_ALERT");
+        }
+    }
+}
